cb1d53d5e2e8be9fd97ae7ef0356798fbe22818a
lokified <lsheldon645@gmail.com>
2022-04-01 14:59:34 +0300

    model classes

---
+++ app/src/main/java/com/loki/dictionary/models/Definitions.java
@@ -0,0 +1,43 @@
+package com.loki.dictionary.models;
+
+import java.util.List;
+
+public class Definitions {
+
+    String definition = "";
+    String example = "";
+    List<String> synonym = null;
+    List<String> antonyms = null;
+
+    public String getDefinition() {
+        return definition;
+    }
+
+    public void setDefinition(String definition) {
+        this.definition = definition;
+    }
+
+    public String getExample() {
+        return example;
+    }
+
+    public void setExample(String example) {
+        this.example = example;
+    }
+
+    public List<String> getSynonym() {
+        return synonym;
+    }
+
+    public void setSynonym(List<String> synonym) {
+        this.synonym = synonym;
+    }
+
+    public List<String> getAntonyms() {
+        return antonyms;
+    }
+
+    public void setAntonyms(List<String> antonyms) {
+        this.antonyms = antonyms;
+    }
+}
